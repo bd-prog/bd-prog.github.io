99a5183102efeda2fc45c2cbb68b3dd048c99046
bd-prog <124307071+bd-prog@users.noreply.github.com>
2024-07-02 11:15:41 +0600

    Update next.config.js
    Signed-off-by: bd-prog <124307071+bd-prog@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -5,5 +5,11 @@
 const nextConfig = {
   reactStrictMode: true,
 };
+basePath:
+    process.env.NODE_ENV === "production"
+      ? "https://bd-prog.github.io"
+      : undefined,
+};
+
 
 module.exports = nextConfig;
